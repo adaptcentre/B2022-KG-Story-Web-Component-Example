6de12aa33738bfe1278aff01d11db251d6a4a3dc
ivanbacher <ivan.bacher@gmail.com>
2022-05-12 07:40:39 +0200

    update packages

--- bundler/webpack.dev.js
+++ bundler/webpack.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge')
 const commonConfiguration = require('./webpack.common.js')
-const ip = require('internal-ip')
+//const {ip} = require('internal-ip')
 const portFinderSync = require('portfinder-sync')
 
 const infoColor = (_message) =>
@@ -14,26 +14,8 @@ module.exports = merge(
         mode: 'development',
         devServer:
         {
-            host: '0.0.0.0',
             port: portFinderSync.getPort(8080),
-            contentBase: './dist',
-            watchContentBase: true,
-            open: true,
-            https: false,
-            useLocalIp: true,
-            disableHostCheck: true,
-            overlay: true,
-            noInfo: true,
-            after: function(app, server, compiler)
-            {
-                const port = server.options.port
-                const https = server.options.https ? 's' : ''
-                const localIp = ip.v4.sync()
-                const domain1 = `http${https}://${localIp}:${port}`
-                const domain2 = `http${https}://localhost:${port}`
-                
-                console.log(`Project running at:\n  - ${infoColor(domain1)}\n  - ${infoColor(domain2)}`)
-            }
+            open: true
         }
     }
 )
