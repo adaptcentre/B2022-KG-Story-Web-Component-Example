b8d3edae9b875a45b4ed05af17a020bb13b2a752
ivanbacher <ivan.bacher@gmail.com>
2022-07-05 09:46:02 +0200

    added stats

--- src/script.js
+++ src/script.js
@@ -1,3 +1,21 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'b2022-kg-story'
+import Stats from 'stats.js'
+
+const stats = new Stats()
+
+document.body.appendChild(stats.dom)
+
+function animate() {
+
+  stats.begin()
+
+  // monitored code goes here
+
+  stats.end()
+
+  requestAnimationFrame(animate)
+}
+
+requestAnimationFrame(animate)
